88bf42aac82c3508d9fc4522e060552512187e5c
Lucas Garbin <162581841+PietroMartiori@users.noreply.github.com>
2025-11-26 10:04:03 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function salvartarefa() {
+    const dados = {
+        prioeidade : "Urgente",
+        descricao : " Entregar trabalho final DW l",
+        recursosNecessario : ["Computador", "Internet", "Exemplos anteriores"],
+        dataLimite : "2025-12-03 00:00:00",
+        matricula : "2025306766"
+    }
+}
