619abb6beb8097a0f87fb3564801a3420fbfc7d1
bogg0 <joaobatistaabc132@gmail.com>
2025-12-03 09:25:58 -0300

    troquei o endpoint denovo
    o professor trocou o endpoint so agora dai mudei pq se nao o codigo da bosta 👍

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,5 +1,5 @@
 const urlBase = 'http://159.65.228.63/';
-const endpoint = 'produtos';
+const endpoint = 'tarefas';
 
 async function salvartarefa() {
     const prioridade = document.getElementById('prioridade').value;
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const urlBase = 'http://159.65.228.63/';
-const endpoint = 'produtos';
+const endpoint = 'tarefas';
 let dados = [];
 
 async function atualizar() {
